42cfa6823523ab9b069ab2a2b4b902d88b809105
maxmugato <114144181+maxmugato@users.noreply.github.com>
2026-05-28 06:58:22 +0100

    Update Password_generator.py

--- Password_generator.py
+++ Password_generator.py
@@ -40,3 +40,6 @@ has_number = input("Do you want to have numbers (y/n)? ").lower() == "y"
 has_special = input("Do you want to have special characters (y/n)? ").lower() == "y"  
 pwd = generate_password(min_length, has_number, has_special)
 print("The generated password is:", pwd)
+
+https://www.youtube.com/watch?v=XCIBOl3FTKo
+
